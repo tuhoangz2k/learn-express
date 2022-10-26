4ec8584f3b84d2b69c0f430629ae97963a2b1a14
Tu Hoang <tuhoangz2k@gmail.com>
2022-10-26 13:09:56 +0700

    sa

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const jwt=require('jsonwebtoken')
 const personRouter= require('./routers/personRouter')
 const PersonModel=require('./models/person')
 const connectdb= require('./db/connectDB')
+const session = require('express-session')
 const app=express();
 const port=3002;
 connectdb()
@@ -13,166 +14,36 @@ connectdb()
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 5000 }
+}))
 app.use(cookieParser())
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'lp.com');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  next();
+app.get('/',(req,res,next) => {
+res.sendFile(path.join(__dirname,'demo-cookies.html'))
 })
-app.use('/public',express.static(path.join(__dirname, 'public')))
 
-
-app.get('/home',(req, res,next)=>{
-  var token=req.cookies.token;
-  try {
-  var decode=jwt.verify(token,'wiburach')
-  } catch (error) {
-    res.redirect('/login')
+app.get('/demo', function(req, res, next) {
+  console.log(req.session);
+  if (req.session.views) {
+    req.session.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<p>views: ' + req.session.views + '</p>')
+    res.write('<p>expires in: ' + (req.session.cookie.maxAge / 1000) + 's</p>')
+    res.end()
+  } else {
+    req.session.views = 1
+    res.end('welcome to the session demo. refresh!')
   }
-  PersonModel.findById(decode._id)
-  .then((data)=>{
-    if(data.role==='manager'){
-     next();
-    }else{
-    return res.redirect('/login')
-    }
-  })
-  .catch((err)=>{res.json('err')})
-  
-} ,(req, res, next) => {
-  res.sendFile(path.join(__dirname, 'home.html'))
-})
-app.get('/login', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'login.html'))
 })
 
-app.post('/login', (req, res, next) => {
-  PersonModel.findOne({name: req.body.username,password: req.body.password})
-  .then((data)=>{
-    if(data){
-      const token=jwt.sign({_id:data._id},'wiburach')
-      return res.json({message:'successful',token})
-    }else{
-      return res.json('dang nhap that bai')
-    }
-    
-  })
-  .catch((err)=>{res.status(404).send('khong tim thay')})
+app.get('/logout',(req, res, next)=>{
+  req.session.destroy()
+  res.json('log out')
 })
-
-
-function checklogin(req,res, next){
-  try {
-    const token=req.cookies.token
-    const result=jwt.verify(token,'wiburach')
-    console.log(req.body.token);
-    
-    PersonModel.findById(result._id)
-    .then((data)=>{
-      if(data){
-        req.data=data
-        console.log(data);
-        next()
-      }else{
-        res.redirect('/login')
-      }
-    })
-    .catch((err)=>{res.json('err');})
-    
-  } catch (error) {
-    return res.json('you need to login')
-  }
-}
-
-function checkTask(req, res, next){
-const role =req.data.role
-console.log(role);
-if(role==='student'||role==='teacher'||role==='manager'){
-  next()
-}else{
-  res.json('not permitson')
-}
-}
-
-function checkStudentPage(req, res, next){
-  const role =req.data.role
-  console.log('student',role);
-  if(role==='teacher'||role==='manager'){
-    next()
-  }else{
-    res.json('not permitson')
-  }
-  }
-
-
-  function checkTeacherPage(req, res, next){
-    const role =req.data.role
-    console.log('student',role);
-    if(role==='manager'){
-      next()
-    }else{
-      res.json('not permitson')
-    }
-    }
-
-app.patch('/edit', (req, res, next)=>{
-  const token=req.headers.cookie.split('=')[1]
-  try {
-  var decode=jwt.verify(token,'wiburach')
-  } catch (error) {
-    res.redirect('/login')
-  }
-  PersonModel.findById(decode._id)
-  .then((data)=>{
-    if(data.role==='manager'){
-     next();
-    }else{
-    return res.redirect('/login')
-    }
-  })
-  .catch((err)=>{res.json('err')})
-  next()
-},(req, res, next)=>{
-  res.json('update successful')
-})
-
-app.use('/task',checklogin,checkTask,(req, res, next)=>{
-  const token=req.data;
-  res.json('all task')
-})
-
-app.use('/student',checklogin,checkStudentPage,(req, res, next)=>{
-  try {
-    const token=req.cookies.token
-    const result=jwt.verify(token,'wiburach')
-    console.log(req.body.token);
-    req.data=result
-    if(result)next();
-  } catch (error) {
-    return res.json('you need to login')
-  }
-},(req, res, next)=>{
-  const token=req.data;
-  PersonModel.findById(token._id)
-  .then((data)=>{
-  res.json({name:data.name,description:'student page'})
-  })
-  .catch((err)=>{res.json('err');})
-})
-
-
-
-app.use('/teacher',checklogin,checkTeacherPage,(req, res, next)=>{
-  const token=req.data;
-  PersonModel.findById(token._id)
-  .then((data)=>{
-  res.json({name:data.name,description:'teacher page'})
-  })
-  .catch((err)=>{res.json('err');})
-})
-
 app.use('/user',personRouter)
 app.listen(port,()=>{
   console.log('listening on port');
